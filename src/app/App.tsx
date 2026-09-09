aab729c9905514c0d61a97b0bb0e42d3a93e944c
Dominik Dragon <dragondominik0103@gmail.com>
2026-09-09 15:43:56 +0200

    chore: Add TailwindCSS and rework folder structure

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import '../global.css';
+
+function App() {
+  return (
+    <main className="container">
+     <h1 className="text-4xl text-red-500 font-bold">Testing TailwindCSS</h1>
+    </main>
+  );
+}
+
+export default App;
