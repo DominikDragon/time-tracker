d554a3a4f20ce34ac97293a7635035878f0d971e
Dominik Dragon <dragondominik0103@gmail.com>
2026-09-12 10:02:54 +0200

    chore: Set up base router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,27 @@
 import { ProjectProvider } from "../context/ProductContext";
 import { TimerProvider } from "../context/TimerContext";
 import { TimerPage } from "../pages/TimerPage";
+import { BrowserRouter ,Routes, Route } from "react-router-dom";
 import "../global.css";
 
 function App() {
     return (
-        <ProjectProvider>
-            <TimerProvider>
-                <TimerPage />
-            </TimerProvider>
-        </ProjectProvider>
+        <BrowserRouter>
+            <ProjectProvider>
+                <TimerProvider>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<TimerPage/>}
+                        />
+                    </Routes>
+                    <div className="flex flex-row gap-6 mt-10">
+                        <a href="/projects">Projects</a>
+                        <a href="/">Timer</a>
+                    </div>
+                </TimerProvider>
+            </ProjectProvider>
+        </BrowserRouter>
     );
 }
 
